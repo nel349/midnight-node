a94f1df8f37fcd9f4b7d3d33fbbd7bfafa4608d5
Oscar Bailey <79094698+ozgb@users.noreply.github.com>
2026-01-13 14:25:43 +0000

    chore(deps): update toolkit with compact 0.28.0-rc.0 (#468)
    * chore(deps): update compact-js to 2.4.0-rc.0
    
    * chore: rebuild undeployed genesis
    
    * chore: add change file
    
    * test: fix incorrect seed value in test

---
+++ changes/changed/toolkit-update-compact-028.md
@@ -0,0 +1,4 @@
+# Update toolkit for compatibility with compact 0.28.0-rc.0
+
+PR: https://github.com/midnightntwrk/midnight-node/pull/468
+Ticket: https://shielded.atlassian.net/browse/PM-21174
--- util/toolkit/src/commands/show_wallet.rs
+++ util/toolkit/src/commands/show_wallet.rs
@@ -188,7 +188,7 @@ mod tests {
 			if !utxos.is_empty() && !coins.is_empty() && !dust_utxos.is_empty();
 		"funded-unshielded-seed-3"
 	)]
-	#[test_case(test_fixture!("a51c86de32d0791f7cffc3bdff1abd9bb54987f0ed5effc30c936dddbb9afd9d", "genesis/genesis_block_undeployed.mn") =>
+	#[test_case(test_fixture!("a51c86de32d0791f7cffc3bdff1abd9bb54987f0ed5effc30c936dddbb9afd9d530c8db445e4f2d3ea42a321b260e022aadf05987c9a67ec7b6b6ca1d0593ec9", "genesis/genesis_block_undeployed.mn") =>
 	matches Ok(ShowWalletResult::Json(WalletInfoJson {utxos, coins, dust_utxos}))
 			if !utxos.is_empty() && !coins.is_empty() && !dust_utxos.is_empty();
 		"funded-unshielded-seed-4"
